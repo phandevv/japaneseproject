edc506d1cc17bd42ac081869a2deac3721f91587
phandeptrai <vanphan190704@gmail.com>
2026-07-20 08:28:25 +0700

    fix: migrate to BrowserRouter (remove URL hash) and fix scrollbar flickering layout shift

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.jsx'
@@ -30,9 +30,9 @@ createRoot(document.getElementById('root')).render(
       <LanguageProvider>
         <ThemeProvider>
           <AuthProvider>
-            <HashRouter>
+            <BrowserRouter>
               <App />
-            </HashRouter>
+            </BrowserRouter>
           </AuthProvider>
         </ThemeProvider>
       </LanguageProvider>
